579ad3a35bdd55d3316a3c145611121aac66b4d8
khparkDev <pkh1620@naver.com>
2015-11-23 15:01:48 +0900

    .

--- LogfileAnalysisTool/src/main/java/com/khpark/log/analysis/base/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.khpark.log.analysis.base;
-
-public class Constants {
-	public static final String EXP_URI_START = "RPC START \\[ [(a-zA-Z)*]+ , [(a-zA-Z)*]+ \\]";
-	public static final String EXP_URI_END = "RPC END \\[ [(a-zA-Z)*]+ , [(a-zA-Z)*]+ \\]";
-	public static final String EXP_PARAMS_START = "- array \\(";
-	public static final String EXP_PARAMS_END = "\\)";
-}
--- LogfileAnalysisTool/src/main/java/com/khpark/log/analysis/service/PatternType.java
+++
@@ -1,5 +0,0 @@
-package com.khpark.log.analysis.service;
-
-public enum PatternType {
-	URI_START, URI_END, INPUT_PARAMS
-}
